46130ff45c8f7050df5a9785162c7adb2de046ac
sofr92 <129745948+sofr92@users.noreply.github.com>
2025-05-20 19:54:28 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,25 @@
 // app/layout.tsx
+
 export const metadata = {
-  title: 'Husleiekalkulator',
-  description: 'Få oversikt over utgifter ved leie og boligkjøp',
+  title: "Husleiekalkulator",
+  description: "Få oversikt over utgifter ved leie og boligkjøp",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="no">
-      <head />
-      <body>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body style={{ fontFamily: "'Manrope', sans-serif" }}>{children}</body>
     </html>
   );
 }
